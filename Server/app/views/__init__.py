ac0d5a6629775ab057b047e6ce30426796758f2b
bak_GyuJun <iopasd753951@gmail.com>
2018-11-21 19:13:30 +0900

    [Refactoring]

--- Server/app/views/__init__.py
+++ Server/app/views/__init__.py
@@ -92,7 +92,7 @@ class BaseResource(Resource):
         self.now = time.strftime('%Y-%m-%d %H:%M:%S')
 
     @classmethod
-    def unicode_safe_json_response(cls, data, status_code=200):
+    def unicode_safe_json_response(cls, data, status_code=201):
         return Response(
             ujson.dumps(data, ensure_ascii=False),
             status_code,
